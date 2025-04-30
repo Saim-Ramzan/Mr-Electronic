8dbff18c2d04d193be27febdf818a5d73f73d847
SaimRamzan <saimramzan5353@gmail.com>
2025-04-30 13:03:14 +0500

    image config

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,7 @@
 module.exports = {
   images: {
-    domains: ['i.ibb.co','https://ibb.co'], 
+    // domains: ['i.ibb.co','https://ibb.co'], 
+    domains: ['i.ibb.co'],
   },
 };
 
-
- // {
-    //   "id":2,
-    //   "FeatureService": "Smartphone Repairing",
-    //   "image": "https://ibb.co/8L0ChBy5",
-    //   "buttonName": "Explore"
-    // },
-    // {
-    //   "id":3,
-    //   "FeatureService": "Tablet Repairing",
-    //   "image": "https://ibb.co/Wp34xbLb",
-    //   "buttonName": "Explore"
-    // },
-    // {
-    //   "id":4,
-    //   "FeatureService": "Battery Replacement",
-    //   "image": "https://ibb.co/9LJ9G2S",
-    //   "buttonName": "Explore"
-    // },
-    // {
-    //   "id":5,
-    //   "FeatureService": "Screen Replacement",
-    //   "image": "https://ibb.co/Ldss5z7r",
-    //   "buttonName": "Explore"
-    // },
-    // {
-    //   "id":6,
-    //   "FeatureService": "Car Repairing",
-    //   "image": "https://ibb.co/MDB3P0QB",
-    //   "buttonName": "Explore"
-    // }
